6479d744b10fa6b91295bafdcc40a4a97930e96e
Stephen Legge <stephen@aleggeup.com>
2017-11-28 06:41:58 +0000

    Init script to install required dependencies

---
+++ scripts/x-tools/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf update
+dnf install make bison flex gperf libtool texinfo gcc gcc-c++ gmp-devel cvs patch lzma ncurses ncurses-devel wget help2man glibc-static libstdc++-static file
