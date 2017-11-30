5b496b0395dd66db58c6601a5e09ec724ae9561c
Stephen Legge <stephen@aleggeup.com>
2017-11-30 07:12:34 +0000

    Increase amount of swap space

--- scripts/swapon.sh
+++ scripts/swapon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 free -h
-dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
+dd if=/dev/zero of=/var/swap.img bs=1024k count=4000
 mkswap /var/swap.img
 swapon /var/swap.img
 free -h
