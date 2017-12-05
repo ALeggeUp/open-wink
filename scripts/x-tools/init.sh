deff1c3aaa00909b69c8a89d2218dae1b7ab7909
Stephen Legge <stephen@aleggeup.com>
2017-12-05 07:00:08 +0000

    Add pod2html dependency

--- scripts/x-tools/init.sh
+++ scripts/x-tools/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf update
-dnf install make bison flex gperf libtool texinfo gcc gcc-c++ gmp-devel cvs patch lzma ncurses ncurses-devel wget help2man glibc-static libstdc++-static file
+dnf install make bison flex gperf libtool texinfo gcc gcc-c++ gmp-devel cvs patch lzma ncurses ncurses-devel wget help2man glibc-static libstdc++-static file perl-Pod-Html
